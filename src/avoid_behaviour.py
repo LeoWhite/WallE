638cd63d6a178effc289ea9730d4db6c9d06a98f
Leo White <leo.white@mybigideas.co.uk>
2019-03-15 15:29:09 +0000

    Tweaked speed values, and distances, to be in the expected range

--- src/avoid_behaviour.py
+++ src/avoid_behaviour.py
@@ -10,29 +10,32 @@ class ObstacleAvoidingBehavior(object):
         self.max_distance = 100
 
     def get_speeds(self, nearest_distance):
-        if nearest_distance > 100:
-            nearest_speed = 100
-            furthest_speed = 100
+        if nearest_distance > 500:
+            nearest_speed = 0.75
+            furthest_speed = 0.75
             delay = 100
-        elif nearest_distance > 50:
-            nearest_speed = 100
-            furthest_speed = 80
+        elif nearest_distance > 450:
+            nearest_speed = 0.75
+            furthest_speed = 0.5
             delay = 100
-        elif nearest_distance > 20:
-            nearest_speed = 100
-            furthest_speed = 60
+        elif nearest_distance > 350:
+            nearest_speed = 0.75
+            furthest_speed = 0.25
             delay = 100
-        elif nearest_distance > 10:
-            nearest_speed = -40
-            furthest_speed = -100
+        elif nearest_distance > 150:
+            nearest_speed = 0.6
+            furthest_speed = -0.6
             delay = 100
         else: # collison
-            nearest_speed = -100
-            furthest_speed = -100
+            nearest_speed = -0.75
+            furthest_speed = -0.75
             delay = 250
         return nearest_speed, furthest_speed, delay
 
     def run(self):
+        # Sleep for a second to allow the distance sensor to settle down
+        sleep(1)
+        
         while True:
             # Get the sensor readings
             distance = self._WallE.get_distance()
@@ -43,11 +46,11 @@ class ObstacleAvoidingBehavior(object):
                 "Delays: l", delay)
 
             # Send this to the motors
-            #self._WallE.set_left(nearest_speed)
-            #self._WallE.set_right(furthest_speed)
+            self._WallE.set_left(nearest_speed)
+            self._WallE.set_right(furthest_speed)
 
             # Wait our delay time
-            sleep(delay * 0.001)
+            sleep(0.1)
 
 behavior = ObstacleAvoidingBehavior(WallE())
 behavior.run()
