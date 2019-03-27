4ae5a10a8ede420a7f74b8a3f6e36f5f9ab2c125
Leo White <leo.white@mybigideas.co.uk>
2019-03-27 22:03:11 +0000

    Trigger audio on button press

--- Tests/buttonTest.py
+++ Tests/buttonTest.py
@@ -1,6 +1,33 @@
 from gpiozero import Button
+import pygame
+import time
+import os
+
+os.environ["SDL_VIDEODRIVER"] = "dummy" # Removes the need to have a GUI window
+pygame.init()
+#pygame.display.set_mode((1,1))
+
+
+pygame.mixer.init()
+pygame.mixer.music.set_volume(1.0)
+
+musicAudioChannel = None
+musicAudio = pygame.mixer.Sound("/home/pi/Audio/PutOnYourSundayClothes.wav")
+musicAudio.set_volume(0.2)
+
+def playButtonCallback():
+  global musicAudioChannel
+  
+  if musicAudioChannel == None or False == musicAudioChannel.get_busy():
+    musicAudioChannel = musicAudio.play()
+  else:
+    musicAudioChannel.stop()
+
 
 button = Button(23)
-button.wait_for_press()
-print("The button was pressed!")
+button.when_pressed = playButtonCallback
+
+while True:
+  time.sleep(1)
+  
 
